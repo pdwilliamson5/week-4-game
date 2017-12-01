cb6739d623c81489a52603023f404a2ea60f292f
pdwilliamson5 <30446985+pdwilliamson5@users.noreply.github.com>
2017-12-01 16:40:05 -0600

    Add files via upload

---
+++ week-4-game/game.js
@@ -0,0 +1,89 @@
+
+var random_result
+var win = 0;
+var loss = 0;
+var previous = 0;
+
+// start and reset
+var resetAndStart = function() {
+	$(".gems").empty();
+
+	var images =[
+	'assets/images/red.png',
+	'assets/images/blue2.png',
+	'assets/images/pink.jpg',
+	'assets/images/green.png'];
+
+// random number counter
+ random_result = Math.floor(Math.random() * 19) + 111;
+
+$("#result").html('Random Result:  ' + random_result);
+
+
+// gems random number
+
+for(var i = 0; i < 4; i++){
+var random = Math.floor(Math.random() * 12) + 1;
+
+var gem = $("<div>");
+	gem.attr({"class": 'gem', "number-random": random});
+
+	gem.css({
+		"background-image":"url('" + images[i] + "')",
+		"background-size":"cover"
+	});
+
+
+
+$(".gems").append(gem);
+
+	
+	}
+}
+
+resetAndStart();
+var reset = function(){
+
+}
+
+// gem button function
+
+$(document).on('click', ".gem" ,function(){
+
+	var num = parseInt($(this).attr('number-random'));
+
+
+	// win/loss counter
+
+	previous += num;
+
+	$('#total').html( "Your Score! " + previous)
+
+	if(previous > random_result){
+		loss --;
+		$("#loss").html("loss " + loss);
+
+		previous = 0;
+
+		$('#total').html(previous);
+
+			resetAndStart();
+
+		}
+		else if(previous === random_result){
+			win ++;
+			$("#win").html( "wins " + win);
+
+			$('#total').html(previous);
+
+
+			previous = 0;
+
+			resetAndStart();
+		}
+	
+
+	console.log(previous); 
+
+});
+
